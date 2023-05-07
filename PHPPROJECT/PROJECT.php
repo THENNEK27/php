15037f5fdccb9deced62e5dd6a0c83776d39599c
THENNEK27 <132435666+THENNEK27@users.noreply.github.com>
2023-05-07 21:36:02 +0800

    Add files via upload

---
+++ PHPPROJECT/PROJECT.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Resources">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cabin+Sketch&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bruno+Ace&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Satisfy&family=Secular+One&display=swap" rel="stylesheet">
+
+    <title>PHP_PROJECT</title>
+</head>
+<body>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <div class=container>
+        <div class="nav">
+            <img src="Resources/Viva Marinduque LOGO.jpg" class="logo">
+            <nav>
+                <ul>
+                    <li><a href="PROJECT.php">Home</a></li>
+                    <li><a href="Boacpage/boac.html">Town</a></li>
+                    <li><a href="About">About</a></li>
+                    <li><a href="Blog">Blog</a></li>
+                </ul>
+            </nav>
+            <img src="Resources/grid-outline.svg" class="menu-icon">
+        </div>
+        <div class="row">
+            <div class="col">
+                <h1>Marinduque Tourist Guide</h1>
+                <p>Welcome to Marinduque the heart of the Philippines</p>
+            </div>
+            <div class="search-bar">
+                <form>
+                    <input type="search" placeholder="Search...">
+                    <button type="submit">Enter</button>
+                </form>  
+            </div>
+            <div class="Container">
+                <div class="left-context">
+                    <h2>Magandang Buhay, Travelers,Welcome to Marinduque</h2>
+                    <p>Start planning your next getaway to Marinduque and 
+                    explore Marinduque tours to know the exciting things to do in Marinduque 
+                    and get the most of your island getaway!</p>
+                    <p>Visit Boac and Mogpog to learn more about these fascinating tourist destinations, 
+                       which will provide you with amazing stories to share with your friends and family.
+                    </p>
+                </div>
+
+            </div>
+            <div class="coldown">
+                <div class="card boac">
+                    <h5>Boac</h5>
+                    <p>The Capital of Marinduque</p>
+                    <a href="Boacpage/boac.html"><button class="btn-Boac">Explore Boac</button></a>
+                </div>
+            </div>
+            <div class="card mogpog">
+                    <h5>Mogpog</h5>
+                    <p>The Home of Orignal Morions</p>
+                    <a href="Mogpogpage/mogpog.php"><button class="btn-Mogpog">Explore Mogpog</button></a>
+                </div>
+        </div>
+        
+    </div>
+</body>
+</html>
